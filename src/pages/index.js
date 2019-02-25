b95d59c763eb9e7d09b73236c24c0284ffbc418b
Vijit Singh <vijit.singh@mavs.uta.edu>
2019-02-25 12:00:46 -0600

    fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,21 +14,12 @@ const IndexPage = () => (
     <Carousel
 
       autoplay={true}
-      speed={1}
+      speed={.1}
       enableKeyboardControls={false}
       transitionMode={'fade'}
       heightMode={'max'}
       pauseOnHover={true}
-
-      renderBottomCenterControls={({ currentSlide, slideCount }) => (
-        <div>Slide: {currentSlide + 1} of {slideCount}</div>
-      )}
-      renderCenterLeftControls={({ previousSlide }) => (
-        <button onClick={previousSlide}>Previous</button>
-      )}
-      renderCenterRightControls={({ nextSlide }) => (
-        <button onClick={nextSlide}>Next</button>
-      )}
+      wrapAround={true}
     >
         <img src="https://source.unsplash.com/random/3840 x 2160" />
         <img src="https://source.unsplash.com/random/1920x1080" />
